2347407bbc80db45f9491917b022ba9cab43df28
Chamith <chamith.softwaredev@gmail.com>
2025-09-01 16:55:11 +0530

    secure true

--- src/shared/email-sender.service.ts
+++ src/shared/email-sender.service.ts
@@ -10,6 +10,7 @@ export class EmailSenderService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PSWD,
